a5a9e8c8ddbc75ab56d7cd89ee7486316e5b489e
fadhillaislamita <73664604+fadhillaislamita@users.noreply.github.com>
2020-12-21 16:18:43 +0700

    Update masterbarang.java

--- src/kelompok3/masterbarang.java
+++ src/kelompok3/masterbarang.java
@@ -58,4 +58,292 @@ static Connection conn;
             e.printStackTrace();
         }
   }
+	public void TambahBarang() throws Exception {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		
+		barang brg = new barang();
+		
+		System.out.println(" ******************************************* ");
+		System.out.println(" *                TAMBAH DATA              * ");
+		System.out.println(" *******************************************\n");
+		
+		sku = brg.sku();
+		nama = brg.nama();
+		stock = brg.stock();
+		harga_beli = brg.harga_beli();
+		harga_jual = brg.harga_jual();
+       
+        try {
+            stmt = conn.createStatement();
+            String sql = "INSERT INTO barang VALUES ('"+sku+"', '"+nama+"', '"+stock+"', '"+harga_beli+"', '"+harga_jual+"')";   
+
+            stmt.execute(sql);
+            System.out.println("\nData Berhasil Tersimpan");
+    
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }            
+		LihatData();
+        
+	}
+	
+	//Cari Data Barang
+	@Override
+	public void CariBarang() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		
+		ArrayList <barang> data = new ArrayList<>();
+		
+		System.out.println("\n ********************************************* ");
+		System.out.println("*                  CARI  DATA                *");
+		System.out.println(" ********************************************* ");
+		
+		System.out.print("Masukkan nama barang yang ingin di cari : ");
+		String cari = input.nextLine();
+
+		stmt = conn.createStatement();
+		String sql = "SELECT * FROM barang WHERE nama='"+cari+"'";
+		
+		try {
+			rs = stmt.executeQuery(sql);
+			
+        	if(rs.next()) { 
+        		barang n = new barang();
+        		
+        		n.setSku(rs.getString("sku"));
+        		n.setNama(rs.getString("nama"));
+        		n.setStock(rs.getInt("stock"));
+        		n.setHarga_beli(rs.getInt("harga_beli"));
+        		n.setHarga_jual(rs.getInt("harga_jual"));
+        		
+        		data.add(n);
+        		
+				System.out.println(" ");
+         		System.out.print("  SKU");
+         		System.out.print("\t\t");
+         		System.out.print("  NAMA");
+         		System.out.print("\t\t");
+         		System.out.print("  STOCK");
+         		System.out.print("\t\t");
+         		System.out.print("  HARGA BELI");
+         		System.out.print("\t\t");
+         		System.out.println("  HARGA JUAL ");
+         		
+         		for(barang barang : data) {
+    	        	System.out.print("  " +barang.sku);
+    	        	System.out.print("\t\t");
+    	        	System.out.print("  " +barang.nama);
+    	        	System.out.print("\t\t");
+    	        	System.out.print("  " +barang.stock);
+    	        	System.out.print("\t\t");
+    	        	System.out.print("  " +barang.harga_beli);
+    	        	System.out.print("\t\t\t");
+    	        	System.out.println("  " +barang.harga_jual);
+            	}
+        	}
+        	else {
+        		System.out.println("\nBarang Tidak Tersedia");
+        	}
+        	
+            System.out.println("\nCari Barang Lagi? Y/T");
+            System.out.print("Jawaban : ");
+            jwb = input.nextLine();
+            
+            if(jwb.equalsIgnoreCase("y")) {
+            	CariBarang();
+            }
+            else {
+                System.out.println("\nKembali ke Menu? Y/T");
+                System.out.print("Jawaban : ");
+                jwb = input.nextLine();
+                
+                if(jwb.equalsIgnoreCase("y")) {
+                	Menu();
+                }
+                else {
+                
+                }
+            }
+        		
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//Ubah Barang
+	@Override
+	public void UbahBarang() throws Exception {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		
+		String sql;
+		barang brg = new barang();
+		
+        System.out.println("\n *********************************************** ");
+        System.out.println(" *                UPDATE BARANG                *");
+        System.out.println(" *********************************************** ");
+        
+        System.out.println("1. Nama");
+        System.out.println("2. Stock");
+        System.out.println("3. Harga Beli");
+        System.out.println("4. Harga Jual");
+		System.out.print("\nPilih data yang ingin diubah : ");
+		Integer ubah = input1.nextInt();
+		System.out.println(" ");
+		
+        stmt = conn.createStatement();
+        
+        
+        try {
+			switch (ubah) {
+				case 1 : 
+					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
+					this.sku = input.nextLine();
+			        nama = brg.nama();
+			        
+			        sql = "UPDATE barang SET nama='"+nama+"' WHERE sku='"+sku+"'";   
+			        stmt.execute(sql);
+					break;
+				case 2 :
+					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
+					this.sku = input.nextLine();
+			        stock = brg.stock();
+			        
+			        sql = "UPDATE barang SET stock='"+stock+"' WHERE sku='"+sku+"'";   
+			        stmt.execute(sql);
+					break;
+				case 3 :
+					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
+					this.sku = input.nextLine();
+			        harga_beli = brg.harga_beli();
+			        
+			        sql = "UPDATE barang SET harga_beli='"+harga_beli+"' WHERE sku='"+sku+"'";   
+			        stmt.execute(sql);
+					break;
+				case 4 :
+					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
+					this.sku = input.nextLine();
+			        harga_jual = brg.harga_jual();
+			        
+			        sql = "UPDATE barang SET harga_jual='"+harga_jual+"' WHERE sku='"+sku+"'";   
+			        stmt.execute(sql);
+					break;
+				default :
+					System.out.println("Pilihan Tidak Tersedia\n");
+					UbahBarang();
+			}
+
+		}
+        
+        catch (Exception e) {
+        	e.printStackTrace();
+        }			
+		LihatData();
+		
+	}
+	
+	//Hapus Data Barang
+	@Override
+	public void HapusBarang() throws Exception {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		
+		System.out.println("\n ********************************************* ");
+		System.out.println("*                  HAPUS DATA                *");
+		System.out.println(" ********************************************* ");
+		System.out.print("Masukkan SKU pada data yang akan di hapus : ");
+		this.sku = input.nextLine();
+		
+		try {
+			stmt = conn.createStatement();
+			String sql = "DELETE FROM barang WHERE sku='"+sku+"'";
+		
+			stmt.execute(sql);
+			System.out.println("\nData Berhasil Terhapus");
+
+		}
+		
+		catch(Exception e) {
+			e.printStackTrace();
+		}			
+		LihatData();
+
+	}
+	
+	//Melihat Data
+	@Override
+	public void LihatData() throws Exception {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		
+    	stmt = conn.createStatement();
+    	
+    	ArrayList <barang> data = new ArrayList<>();
+        
+        String sql = "SELECT * FROM barang";
+        rs = stmt.executeQuery(sql);
+
+        try {
+            System.out.println("\n ========================*********************************======================== ");
+            System.out.println("   ||                      DATA MASTER BARANG DI TOKO BERKAH                      ||");
+            System.out.println("   ========================*********************************======================== ");
+            
+         		System.out.print("  SKU");
+         		System.out.print("\t\t");
+         		System.out.print("  NAMA");
+         		System.out.print("\t\t");
+         		System.out.print("  STOCK");
+         		System.out.print("\t\t");
+         		System.out.print("  HARGA BELI");
+         		System.out.print("\t\t");
+         		System.out.println("  HARGA JUAL");
+        	
+        	while(rs.next()) { 
+        		barang n = new barang();
+        		
+        		n.setSku(rs.getString("sku"));
+        		n.setNama(rs.getString("nama"));
+        		n.setStock(rs.getInt("stock"));
+        		n.setHarga_beli(rs.getInt("harga_beli"));
+        		n.setHarga_jual(rs.getInt("harga_jual"));
+        		
+        		data.add(n);
+        	}
+
+        	for(barang barang : data) {
+	        	System.out.print("  " +barang.sku);
+	        	System.out.print("\t\t");
+	        	System.out.print("  " +barang.nama);
+	        	System.out.print("\t\t");
+	        	System.out.print("  " +barang.stock);
+	        	System.out.print("\t\t");
+	        	System.out.print("  " +barang.harga_beli);
+	        	System.out.print("\t\t\t");
+	        	System.out.println("  " +barang.harga_jual);
+        	}
+        	
+            System.out.println("\nKembali ke Menu? Y/T");
+            System.out.print("Jawaban : ");
+            jwb = input.nextLine();
+            
+            if(jwb.equalsIgnoreCase("y")) {
+            	Menu();
+            }
+            else {
+    
+            }
+
+        } 
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
 }
