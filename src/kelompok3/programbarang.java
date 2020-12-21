abea44fffa7ef7accc1c12b828fe968f7c6d33ae
fadhillaislamita <73664604+fadhillaislamita@users.noreply.github.com>
2020-12-21 16:20:11 +0700

    Update programbarang.java

--- src/kelompok3/programbarang.java
+++ src/kelompok3/programbarang.java
@@ -1,5 +1,9 @@
 package kelompok3;
 
 public interface programbarang {
-
+public void TambahBarang() throws Exception;
+	public void CariBarang() throws Exception;
+	public void UbahBarang() throws Exception;
+	public void HapusBarang() throws Exception;
+	public void LihatData() throws Exception;
 }
