cbeb1d1075e3e2e18668ace424d2913c78db68e6
nikenrhmdni <73600571+nikenrhmdni@users.noreply.github.com>
2020-12-21 22:16:17 +0700

    Update koneksi.java

--- src/kelompok3/koneksi.java
+++ src/kelompok3/koneksi.java
@@ -1,7 +1,7 @@
 package kelompok3;
 
 public class koneksi {
-public static String URL = "jdbc:mysql://localhost:3307/tb_bpl?serverTimezone=Asia/Jakarta";
+public static String URL = "jdbc:mysql://localhost/tb_bpl?serverTimezone=Asia/Jakarta";
 	public static String USERNAME = "root";
 	public static String PASSWORD = "";	
 }
